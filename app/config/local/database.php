21775d60017fce601d19b87c0079629c2964e324
Maks Surguy <msurguy@gmail.com>
2015-04-06 16:35:00 -0700

    Updated Config

---
+++ app/config/local/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'shop-seo',
+            'username'  => 'root',
+            'password'  => 'root',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		)
+	)
+);
